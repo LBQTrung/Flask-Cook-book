6a8ab990ea918cc636b63e6bb71199df5fd0f981
Lê Bá Quốc Trung <91483513+LBQTrung@users.noreply.github.com>
2023-08-21 08:28:55 +0700

    Chapter 12 for Flask Cook Book Readers

---
+++ Chapter12/app/models.py
@@ -0,0 +1,90 @@
+from . import db
+from flask_login import UserMixin, AnonymousUserMixin
+from flask import current_app
+
+
+class Permission:
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship("User", backref="role")
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            "User": (
+                Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES,
+                True,
+            ),
+            "Moderator": (
+                Permission.FOLLOW
+                | Permission.COMMENT
+                | Permission.WRITE_ARTICLES
+                | Permission.MODERATE_COMMENTS,
+                False,
+            ),
+            "Administrator": (0xFF, False),
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
+
+    def __repr__(self):
+        return f"<Role {self.name}>"
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.role is None:
+            if self.email == current_app.config["FLASKY_ADMIN"]:
+                self.role = Role.query.filter_by(permissions=0xFF).first()
+            if self.role is None:
+                self.role = Role.query.filter_by(default=True).first()
+
+    def can(self, permissions):
+        return (
+            self.role is not None
+            and (self.role.permissions & permissions) == permissions
+        )
+
+    def is_administrator(self):
+        return self.can(Permission.ADMINISTER)
+
+    def __repr__(self):
+        return f"<User {self.email}>"
+
+
+class AnonymousUser(AnonymousUserMixin):
+    def can(self, permissions):
+        return False
+
+    def is_administrator(self):
+        return False
+
+
+from . import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
